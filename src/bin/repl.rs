93f585d043d2a5be436ac56614e89be29861e6d9
Spaceginner <ivan.demian2009@gmail.com>
2023-08-18 21:10:28 +0500

    add a minimalistic REPL mode

---
+++ src/bin/repl.rs
@@ -0,0 +1,34 @@
+use std::io::{self, Write};
+use brainfuck::{Machine, Program};
+
+
+fn prompt(message: &str) -> String {
+    print!("{message}");
+    io::stdout().flush().unwrap();
+
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+
+    buffer
+}
+
+
+fn main() {
+    let mut machine = Machine::default();
+
+    loop {
+        // read the expression
+        let expression = prompt("\n> ");
+
+        // if CTRL + D was hit (EOF) - exit
+        if expression == "" {
+            return;
+        };
+
+        // "compile" the expression
+        let program = Program::from(expression.as_str());
+
+        // execute
+        machine.run(&program);
+    };
+}
